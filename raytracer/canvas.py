5f6d1e7d681ac4df93d87becdec2b8a269da1909
Craig <craig@packback.co>
2020-12-22 11:14:15 -0600

    Added ability to generate ppm file content

---
+++ raytracer/canvas.py
@@ -0,0 +1,63 @@
+import raytracer.colors
+
+MAX_LINE_LEN = 70
+
+class Canvas:
+
+    def __init__(self, width: int, height: int):
+
+        self.width = width
+        self.height = height
+
+        self.canvas = [[raytracer.colors.Color(0, 0, 0)] * height for
+                           _ in range(width)]
+
+    def __repr__(self):
+        return f"Canvas [w={self.width}, h={self.height}]"
+
+    def set(self, x, y, color: raytracer.colors.Color):
+        self.canvas[x][y] = color
+
+    def get(self, x, y) -> raytracer.colors.Color:
+        return self.canvas[x][y]
+
+    @staticmethod
+    def clamp(minval, x, maxval):
+        return min(max(minval, x), maxval)
+
+    def _get_ppm_file_content(self):
+        """Convert the canvas into a string containing the PPM file content"""
+        header = f"P3\n{self.width} {self.height}\n255\n"
+
+
+        content_string = ""
+        for i in range(self.height):
+            row_string = ""
+            for j in range(self.width):
+                r = round(self.clamp(0, self.canvas[j][i].red*255, 255))
+                g = round(self.clamp(0, self.canvas[j][i].green*255, 255))
+                b = round(self.clamp(0, self.canvas[j][i].blue*255, 255))
+
+                row_string += f"{r} {g} {b} "
+
+            split_row_string = ""
+            len_current_row = 0
+            for val in row_string.split():
+
+                if len_current_row + len(str(val)) + 2 < MAX_LINE_LEN:
+                    split_row_string += str(val) + " "
+                    len_current_row += len(str(val)) + 1
+                else:
+                    split_row_string = split_row_string.strip()
+                    split_row_string += "\n" + str(val) + " "
+                    len_current_row = len("\n") + len(str(val)) + len(" ")
+
+            content_string += split_row_string.strip()
+            content_string += "\n"
+
+        return header+content_string
+
+    def to_ppm(self, filename):
+        """Write the canvas out to a file with the given filename"""
+
+        content = self._get_ppm_file_content()
---
+++ tests/test_canvas.py
@@ -0,0 +1,90 @@
+import unittest
+
+import raytracer.canvas
+import raytracer.colors
+
+class TestCanvas(unittest.TestCase):
+
+    def test_empty_canvas(self):
+        """Test that an empty canvas is filled with black pixels"""
+
+        c = raytracer.canvas.Canvas(10, 20)
+
+        self.assertEqual(len(c.canvas), 10)
+        self.assertEqual(len(c.canvas[0]), 20)
+
+        for i in range(10):
+            for j in range(20):
+                self.assertEqual(c.canvas[i][j],
+                                 raytracer.colors.Color(0, 0, 0))
+
+
+    def test_write_to_canvas(self):
+        """Test we can write to and read from a canvas"""
+
+        c = raytracer.canvas.Canvas(10, 20)
+        red = raytracer.colors.Color(1, 0, 0)
+
+        c.set(2, 3, red)
+
+        self.assertEqual(c.canvas[2][3], red)
+
+
+    def test_ppm_file_content(self):
+        """Test the content of the PPM header is correct"""
+
+        c = raytracer.canvas.Canvas(5, 3)
+        c1 = raytracer.colors.Color(1.5, 0, 0)
+        c2 = raytracer.colors.Color(0, 0.5, 0)
+        c3 = raytracer.colors.Color(-0.5, 0, 1)
+
+        c.set(0, 0, c1)
+        c.set(2, 1, c2)
+        c.set(4, 2, c3)
+
+        ppm_content = c._get_ppm_file_content()
+
+        ppm_lines = ppm_content.split("\n")
+
+        self.assertEqual(ppm_lines[0], "P3")
+        self.assertEqual(ppm_lines[1], "5 3")
+        self.assertEqual(ppm_lines[2], "255")
+        self.assertEqual(ppm_lines[3], "255 0 0 0 0 0 0 0 0 0 0 0 0 0 0")
+        self.assertEqual(ppm_lines[4], "0 0 0 0 0 0 0 128 0 0 0 0 0 0 0")
+        self.assertEqual(ppm_lines[5], "0 0 0 0 0 0 0 0 0 0 0 0 0 0 255")
+
+
+    def test_ppm_long_line_splitting(self):
+        """Test that we split longer lines at or before 70 characters"""
+
+        c = raytracer.canvas.Canvas(10, 2)
+        for i in range(c.width):
+            for j in range(c.height):
+                c.set(i, j, raytracer.colors.Color(1, 0.8, 0.6))
+
+        ppm_content = c._get_ppm_file_content()
+        ppm_lines = ppm_content.split("\n")
+
+        self.assertEqual(ppm_lines[3],
+            "255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204")
+        self.assertEqual(ppm_lines[4],
+            "153 255 204 153 255 204 153 255 204 153 255 204 153")
+        self.assertEqual(ppm_lines[5],
+            "255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204")
+        self.assertEqual(ppm_lines[6],
+            "153 255 204 153 255 204 153 255 204 153 255 204 153")
+
+
+    def test_ppm_ends_with_newline(self):
+        """Satisfy picky image viewers by ensuring the ppm file ends in a
+        newline
+        """
+
+        c = raytracer.canvas.Canvas(5, 3)
+        ppm_content = c._get_ppm_file_content()
+        self.assertEqual(ppm_content[-1:], "\n")
+
+
+
+if __name__ == "__main__":
+    unittest.main()
