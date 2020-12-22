e9cb6b62bd31aeee99ee276f0f63b8bba443740f
Craig <craig@packback.co>
2020-12-22 17:03:22 -0600

    Added scaling and translation

--- raytracer/transforms.py
+++ raytracer/transforms.py
@@ -11,7 +11,7 @@ class Identity(raytracer.matrices.Matrix):
             self.set(i, i, 1)
 
 
-class Translation(Identity):
+class Translate(Identity):
 
     def __init__(self, x, y, z):
         """ The translation matrix translates objects by an amount x, y, z.
@@ -22,9 +22,24 @@ class Translation(Identity):
           0 0 1 z
           0 0 0 1 ]
         """
-
         super().__init__(4)
-
         self.set(0, 3, x)
-        self.set(0, 3, y)
-        self.set(0, 3, z)
+        self.set(1, 3, y)
+        self.set(2, 3, z)
+
+
+class Scale(Identity):
+
+    def __init__(self, x, y, z):
+        """ The scaling matrix scales objects by a factor x, y, z along those
+        axes. It has the following shape:
+
+        [ x 0 0 0
+          0 y 0 0
+          0 0 z 0
+          0 0 0 1 ]
+        """
+        super().__init__(4)
+        self.set(0, 0, x)
+        self.set(1, 1, y)
+        self.set(2, 2, z)
--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -1,9 +1,13 @@
 import unittest
 
-import raytracer.exceptions, raytracer.matrices, raytracer.transforms
+import raytracer.exceptions
+import raytracer.matrices
+import raytracer.transforms
+import raytracer.points
+import raytracer.vectors
 
-class TestTransforms(unittest.TestCase):
-
+class TestIdentity(unittest.TestCase):
+    """Tests on the identity matrix"""
 
     def test_identity_matrix_mult(self):
         """Test we can identify the identity matrix by a matrix and tuple"""
@@ -28,6 +32,63 @@ class TestTransforms(unittest.TestCase):
         ident = raytracer.transforms.Identity(5)
         self.assertEqual(ident.transpose(), ident)
 
+
+class TestTranslate(unittest.TestCase):
+    """Tests on the translating matrix"""
+
+    def test_translate_point(self):
+        """Test that if we multiply a translate matrix by a point it moves it"""
+
+        p = raytracer.points.Point(-3, 4, 5)
+        T = raytracer.transforms.Translate(5, -3, 2)
+
+        p2 = T * p
+        self.assertEqual(p2, raytracer.points.Point(2, 1, 7))
+
+        # Translating by the inverse of the translate matrix moves the
+        # opposite direction
+
+        p3 = T.inverse() * p
+        self.assertEqual(p3, raytracer.points.Point(-8, 7, 3))
+
+    def test_translate_vector_is_noop(self):
+        """Verify that translating a vector has no effect"""
+
+        v = raytracer.vectors.Vector(1, 2, 3)
+        T = raytracer.transforms.Translate(5, -3, 2)
+
+        v2 = T * v
+        self.assertEqual(v, v2)
+
+
+class TestScale(unittest.TestCase):
+    """Tests on the scaling matrix"""
+
+    def test_simple_scaling(self):
+        """Test that we can scale a point"""
+
+        S = raytracer.transforms.Scale(2, 3, 4)
+        p = raytracer.points.Point(-4, 6, 8)
+        v = raytracer.vectors.Vector(-4, 6, 8)
+
+        p2 = S * p
+        self.assertEqual(p2, raytracer.points.Point(-8, 18, 32))
+
+        p3 = S * v
+        self.assertEqual(p3, raytracer.vectors.Vector(-8, 18, 32))
+
+        p4 = S.inverse() * p
+        self.assertEqual(p4, raytracer.points.Point(-2, 2, 2))
+
+
+    def test_scaling_reflection(self):
+        """Test we can reflect ap oint about an exis using the scaling matrix"""
+
+        S = raytracer.transforms.Scale(-1, 1, 1)
+        p = raytracer.points.Point(-4, 6, 8)
+
+        p2 = S * p
+        self.assertEqual(p2, raytracer.points.Point(4, 6, 8))
 
 if __name__ == "__main__":
     unittest.main()
