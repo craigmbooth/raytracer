3c26c5c9b03146f471eddb3885aa0c41a896daa5
Craig <craig@packback.co>
2020-12-22 09:17:13 -0600

    Refactored and added dot and cross products

--- raytracer/tuples.py
+++ raytracer/tuples.py
@@ -1,79 +1,149 @@
 import math
 
-class IncompatibleTypeError(Exception):
+class IncompatibleLengthError(Exception):
     pass
 
 class Tuple:
+    """ This class represents a generic tuple.
+    """
 
-    def __init__(self, x, y, z, w):
+    def __init__(self, fillables, *args):
+        """Initialize with a list of fillable items, and then one arg
+        with the value per item.  For example, a 2d tuple with x and y
+        coordinates would be initialized as Tuple(["x", "y"], 1, 2). The
+        x and y coordinates are then availble at `obj.x` and `obj.y`
+        """
 
-        self.x = x
-        self.y = y
-        self.z = z
-        self.w = w
-        if self.w == 0:
-            self.type = "vector"
-        elif self.w == 1:
-            self.type = "point"
-        else:
-            self.type = "unknown"
+        if len(fillables) != len(args):
+            raise IncompatibleLengthError
+
+        self.fillables = fillables
+        for fillable, arg in zip(fillables, args):
+            setattr(self, fillable, arg)
 
     def __repr__(self):
-        return f"{self.type} [{self.x}, {self.y}, {self.z}]"
-
-    @classmethod
-    def vector(cls, x, y, z):
-        return cls(x, y, z, 0)
-
-    @classmethod
-    def point(cls, x, y, z):
-        return cls(x, y, z, 1)
-
-    def magnitude(self):
-        return math.sqrt(self.x**2 + self.y**2 + self.z**2)
+        return f"Tuple [{self.x}, {self.y}, {self.z}]"
 
     def __add__(self, other):
-        return Tuple(
-            self.x+other.x,
-            self.y+other.y,
-            self.z+other.z,
-            self.w+other.w)
+
+        if len(self.fillables) != len(other.fillables):
+            raise IncompatibleLengthError
+
+        output_fillables = [
+            getattr(self, f[0]) + getattr(other, f[1]) for
+            f in zip(self.fillables, other.fillables)]
+
+        return Tuple(self.fillables, *output_fillables)
 
     def __sub__(self, other):
-        return Tuple(
-            self.x-other.x,
-            self.y-other.y,
-            self.z-other.z,
-            self.w-other.w)
+
+        if len(self.fillables) != len(other.fillables):
+            raise IncompatibleLengthError
+
+        output_fillables = [
+            getattr(self, f[0]) - getattr(other, f[1]) for
+            f in zip(self.fillables, other.fillables)]
+
+        return Tuple(self.fillables, *output_fillables)
 
     def __neg__(self):
-        return Tuple(
-            -self.x,
-            -self.y,
-            -self.z,
-            -self.w)
+
+        output_fillables = [
+            -getattr(self, f[0]) for f in self.fillables]
+
+        return Tuple(self.fillables, *output_fillables)
+
 
     def __mul__(self, other):
-        return Tuple(
-            self.x * other,
-            self.y * other,
-            self.z * other,
-            self.w * other)
+
+        output_fillables = [
+            getattr(self, f[0]) * other for f in self.fillables]
+
+        return Tuple(self.fillables, *output_fillables)
 
     def __truediv__(self, other):
-        return Tuple(
-            self.x / other,
-            self.y / other,
-            self.z / other,
-            self.w / other)
 
+        output_fillables = [
+            getattr(self, f[0]) / other for f in self.fillables]
+
+        return Tuple(self.fillables, *output_fillables)
 
     @staticmethod
     def _close(x, y, epsilon=1e-6):
+        """Utility function, returns True if two numbers are close, else false
+        """
         return True if abs(x-y) < epsilon else False
 
     def __eq__(self, other):
-        return (self._close(self.x, other.x) and
-                    self._close(self.y, other.y) and
-                    self._close(self.z, other.z) and
-                    self.type == other.type)
+
+        if len(self.fillables) != len(other.fillables):
+            return False
+
+        for f1, f2 in zip(self.fillables, other.fillables):
+            if ((getattr(self, f1) != getattr(other, f2)) or
+                (f1 != f2)):
+                return False
+        else:
+            return True
+
+    def magnitude(self):
+        """Return the magnitude of the tuple"""
+
+        output_fillables = [
+            getattr(self, f[0]) ** 2 for f in self.fillables]
+
+        return math.sqrt(sum(output_fillables))
+
+    def normalize(self):
+        """Normalizes the length of the components to 1"""
+
+        magnitude = self.magnitude()
+
+        output_fillables = [
+            getattr(self, f[0]) / magnitude for f in self.fillables]
+
+        return Tuple(self.fillables, *output_fillables)
+
+
+class Point(Tuple):
+    """This class represents a point in space.  It is initialized with
+    x, y, and z coordinates and then in this class we add a fourth component,
+    w, which is 1 for a point
+    """
+
+    def __init__(self, x, y, z):
+        super().__init__(["x", "y", "z", "w"], x, y, z, 1)
+
+    def __repr__(self):
+        return f"Point [{self.x}, {self.y}, {self.z}]"
+
+
+class Vector(Tuple):
+    """This class represents a vector between two points in space.  It is
+    initialized with x, y, and z coordinates and then in this class we add a
+    fourth component, w, which is 0 for a point
+    """
+
+    def __init__(self, x, y, z):
+        super().__init__(["x", "y", "z", "w"], x, y, z, 0)
+
+    def __repr__(self):
+        return f"Vector [{self.x}, {self.y}, {self.z}]"
+
+    def dot(self, other):
+        """Calculates the dot product between two vectors.  Note that this
+        calculation only makes sense on vectors, so lives on the subclass and
+        also makes direct use of the fillable names
+        """
+
+        return sum([self.x * other.x, self.y * other.y, self.z * other.z])
+
+    def cross(self, other):
+        """Calculates the cross product between two tuples.  Note that this
+        calculation only makes sense on vectors, so lives on the subclass and
+        also makes direct use of the fillable names
+        """
+
+        return Vector(self.y * other.z - self.z * other.y,
+                      self.z * other.x - self.x * other.z,
+                      self.x * other.y - self.y * other.x)
--- tests/test_tuples.py
+++ tests/test_tuples.py
@@ -4,23 +4,22 @@ import unittest
 import raytracer.tuples
 
 class TestTuples(unittest.TestCase):
+    """Tests on the Tuple class"""
 
     def test_point__tuple(self):
         """Test that we can initialize and read from a point as a tuple"""
 
-        p = raytracer.tuples.Tuple(4.3, -4.2, 3.1, 1)
+        p = raytracer.tuples.Tuple(["x", "y", "z", "w"], 4.3, -4.2, 3.1, 1)
 
         self.assertEqual(p.x, 4.3)
         self.assertEqual(p.y, -4.2)
         self.assertEqual(p.z, 3.1)
         self.assertEqual(p.w, 1)
-        self.assertEqual(p.type, "point")
-
 
     def test_vector__tuple(self):
         """Test that we can initialize and read from a vector as a tuple"""
 
-        v = raytracer.tuples.Tuple(4.3, -4.2, 3.1, 0)
+        v = raytracer.tuples.Tuple(["x", "y", "z", "w"], 4.3, -4.2, 3.1, 0)
 
         self.assertEqual(v.x, 4.3)
         self.assertEqual(v.y, -4.2)
@@ -30,19 +29,17 @@ class TestTuples(unittest.TestCase):
     def test_point(self):
         """Test that we can initialize and read from a point"""
 
-        p = raytracer.tuples.Tuple.point(4.3, -4.2, 3.1)
+        p = raytracer.tuples.Point(4.3, -4.2, 3.1)
 
         self.assertEqual(p.x, 4.3)
         self.assertEqual(p.y, -4.2)
         self.assertEqual(p.z, 3.1)
         self.assertEqual(p.w, 1)
-        self.assertEqual(p.type, "point")
 
-
-    def test_vector__tuple(self):
+    def test_vector(self):
         """Test that we can initialize and read from a vector as a tuple"""
 
-        v = raytracer.tuples.Tuple.vector(4.3, -4.2, 3.1)
+        v = raytracer.tuples.Vector(4.3, -4.2, 3.1)
 
         self.assertEqual(v.x, 4.3)
         self.assertEqual(v.y, -4.2)
@@ -53,82 +50,129 @@ class TestTuples(unittest.TestCase):
     def test_addition(self):
         """test that we can add a vector to a point"""
 
-        a1 = raytracer.tuples.Tuple.point(3, -2, 5)
-        a2 = raytracer.tuples.Tuple.vector(-2, 3, 1)
+        a1 = raytracer.tuples.Point(3, -2, 5)
+        a2 = raytracer.tuples.Vector(-2, 3, 1)
 
         a3 = a1 + a2
 
-        self.assertEqual(a3, raytracer.tuples.Tuple.point(1, 1, 6))
-        self.assertEqual(a3, raytracer.tuples.Tuple(1, 1, 6, 1))
+        self.assertEqual(a3,
+            raytracer.tuples.Tuple(["x", "y", "z", "w"], 1, 1, 6, 1))
+        self.assertEqual(a3, raytracer.tuples.Point(1, 1, 6))
 
 
     def test_subtraction__vector_point(self):
         """test that we can subtract a vector from a point"""
 
-        a1 = raytracer.tuples.Tuple.point(3, 2, 1)
-        a2 = raytracer.tuples.Tuple.vector(5, 6, 7)
+        a1 = raytracer.tuples.Point(3, 2, 1)
+        a2 = raytracer.tuples.Vector(5, 6, 7)
 
         a3 = a1 - a2
 
-        self.assertEqual(a3, raytracer.tuples.Tuple.point(-2, -4, -6))
+        self.assertEqual(a3, raytracer.tuples.Point(-2, -4, -6))
 
 
     def test_subtraction__vector_vector(self):
         """test that we can subtract two vectors"""
 
-        a1 = raytracer.tuples.Tuple.vector(3, 2, 1)
-        a2 = raytracer.tuples.Tuple.vector(5, 6, 7)
+        a1 = raytracer.tuples.Vector(3, 2, 1)
+        a2 = raytracer.tuples.Vector(5, 6, 7)
 
         a3 = a1 - a2
 
-        self.assertEqual(a3, raytracer.tuples.Tuple.vector(-2, -4, -6))
+        self.assertEqual(a3, raytracer.tuples.Vector(-2, -4, -6))
 
 
     def test_negation(self):
         """Test that we can negate a vector"""
 
-        a1 = raytracer.tuples.Tuple(1, -2, 3, -4)
+        a1 = raytracer.tuples.Tuple(["a", "b", "c", "d"], 1, -2, 3, -4)
+
         a2 = -a1
-        self.assertEqual(a2, raytracer.tuples.Tuple(-1, 2, -3, 4))
+
+        self.assertEqual(a2,
+            raytracer.tuples.Tuple(["a", "b", "c", "d"], -1, 2, -3, 4))
 
 
     def test_scalar_multiplication(self):
         """Test that we can multiple a vector by a scalar"""
 
-        a1 = raytracer.tuples.Tuple(1, -2, 3, -4)
+        a1 = raytracer.tuples.Tuple(["a", "b", "c", "d"], 1, -2, 3, -4)
         a2 = a1 * 3.5
         a3 = a1 * 0.5
 
-        self.assertEqual(a2, raytracer.tuples.Tuple(3.5, -7, 10.5, -14))
-        self.assertEqual(a3, raytracer.tuples.Tuple(0.5, -1, 1.5, -2))
+        self.assertEqual(a2,
+            raytracer.tuples.Tuple(["a", "b", "c", "d"], 3.5, -7, 10.5, -14))
+        self.assertEqual(a3,
+            raytracer.tuples.Tuple(["a", "b", "c", "d"], 0.5, -1, 1.5, -2))
 
     def test_scalar_division(self):
         """Test that we can divide a tuple by a scalar"""
 
-        a1 = raytracer.tuples.Tuple(1, -2, 3, -4)
+        a1 = raytracer.tuples.Tuple(["a", "b", "c", "d"], 1, -2, 3, -4)
+
         a2 = a1 / 2
 
-        self.assertEqual(a2, raytracer.tuples.Tuple(0.5, -1, 1.5, -2))
+        self.assertEqual(a2,
+            raytracer.tuples.Tuple(["a", "b", "c", "d"], 0.5, -1, 1.5, -2))
 
     def test_magnitude(self):
         """Test that we can calculate the magnitude of a vector"""
 
-        a1 = raytracer.tuples.Tuple.vector(1, 2, 3)
+        a1 = raytracer.tuples.Vector(1, 2, 3)
         self.assertEqual(a1.magnitude(), math.sqrt(14))
 
-        a1 = raytracer.tuples.Tuple.vector(-1, -2, -3)
+        a1 = raytracer.tuples.Vector(-1, -2, -3)
         self.assertEqual(a1.magnitude(), math.sqrt(14))
 
-        a1 = raytracer.tuples.Tuple.vector(1, 0, 0)
+        a1 = raytracer.tuples.Vector(1, 0, 0)
         self.assertEqual(a1.magnitude(), 1)
 
-        a1 = raytracer.tuples.Tuple.vector(0, 1, 0)
+        a1 = raytracer.tuples.Vector(0, 1, 0)
         self.assertEqual(a1.magnitude(), 1)
 
-        a1 = raytracer.tuples.Tuple.vector(0, 0, 1)
+        a1 = raytracer.tuples.Vector(0, 0, 1)
         self.assertEqual(a1.magnitude(), 1)
 
+    def test_normalize(self):
+        """Test that we can normalize a vector"""
 
+        a1 = raytracer.tuples.Vector(4, 0, 0)
+        self.assertEqual(a1.normalize(),
+             raytracer.tuples.Vector(1, 0, 0))
+
+        a1 = raytracer.tuples.Vector(0, 4, 0)
+        self.assertEqual(a1.normalize(),
+             raytracer.tuples.Vector(0, 1, 0))
+
+        a1 = raytracer.tuples.Vector(0, 0, 4)
+        self.assertEqual(a1.normalize(),
+             raytracer.tuples.Vector(0, 0, 1))
+
+    def test_magnitude_normalize(self):
+        """Test the magnitude of a normalized vector is 1"""
+
+        a1 = raytracer.tuples.Vector(1, 2, 3)
+        self.assertEqual(a1.normalize().magnitude(), 1)
+
+    def test_dot_product(self):
+        """Test dot product between two vectors"""
+
+        a1 = raytracer.tuples.Vector(1, 2, 3)
+        a2 = raytracer.tuples.Vector(2, 3, 4)
+
+        self.assertEqual(a1.dot(a2), 20)
+
+    def test_cross_product(self):
+        """Test cross product between two vectors"""
+
+        a1 = raytracer.tuples.Vector(1, 2, 3)
+        a2 = raytracer.tuples.Vector(2, 3, 4)
+
+        self.assertEqual(a1.cross(a2),
+                         raytracer.tuples.Vector(-1, 2, -1))
+
+        self.assertEqual(a2.cross(a1),
+                         raytracer.tuples.Vector(1, -2, 1))
 
 
 if __name__ == "__main__":
